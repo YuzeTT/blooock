5887d8df1eebd8cb478e945deb34d185235fee74
YuzeTT <ziv1024@163.com>
2022-07-24 22:46:28 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <Navbar/>
+  <div>
+    <div class="logo">
+      <img src="./assets/images/home.png" alt="">
+    </div>
+    <div>
+      Home
+    </div>
+  </div>
+</template>
+
+<style scoped>
+body {
+  background-image: url("./assets/images/options_background.png");
+}
+
+.logo {
+  margin-top: 30px;
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: center;
+}
+
+.logo img {
+  width: 150px;
+}
+
+/* .mc-input_box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.mc-input {
+  font-family: Mc;
+  outline: none;
+  background-color: #000;
+  border: #A0A0A0 2px solid;
+  color: #FFF;
+  font-size: 1.3rem;
+  padding: 10px;
+  
+} */
+
+/* .mc-button {
+  font-family: Mc;
+  font-size: 1.3rem;
+  padding: 8px 55px;
+  color: #FFF;
+  outline: #000 2px solid;
+  background-color: #6F6F6F;
+  border-left: #AAAAAA 2px solid;
+  border-top: #AAAAAA 2px solid;
+  border-right: #585858 2px solid;
+  border-bottom: #585858 4px solid;
+}
+
+.mc-button:active {
+  color: #FFFFA0;
+  background-color: #7E88BF;
+  border-left: #BEC7FF 2px solid;
+  border-top: #BEC7FF 2px solid;
+  border-right: #5C669D 2px solid;
+  border-bottom: #5C669D 4px solid;
+} */
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="navbar">
+    <div class="logo"><img src="../assets/images/logo.png" alt=""></div>
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 50px;
+  padding: 0 20px;
+  background-color: #FFF;
+}
+
+.logo {
+  height: 25px;
+}
+
+.logo img {
+  width: 100%;
+  height: 100%;
+}
+</style>
