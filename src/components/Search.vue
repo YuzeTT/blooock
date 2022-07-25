3cd8b8eddea88a3fd99941f0093ac1a572dc6640
YuzeTT <ziv1024@163.com>
2022-07-25 17:20:06 +0800

    add search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,51 +3,76 @@ import { ref } from 'vue'
 import { SubdirectoryArrowLeftRound } from '@vicons/material'
 
 const tab = ref(0)
+const input = ref('')
 
 const list = [
-  { name: '百度', url: 'www.baidu.com' },
+  { name: '百度', url: 'http://www.baidu.com/s?wd=' },
   { name: '谷歌', url: 'www.google.com' },
   { name: '必应', url: 'www.bing.com' },
   { name: 'MC Wiki', url: '...' },
-  { name: 'ModMC', url: '...' },
+  { name: 'MC百科', url: '...' },
 ]
 
 const change_tab = (index: number) => {
   tab.value = index
 }
+
+document.onkeydown = function (e) {
+  if (e.altKey && e.key === '1') {
+    tab.value = 0
+  }
+  if (e.altKey && e.key === '2') {
+    tab.value = 1
+  }
+  if (e.altKey && e.key === '3') {
+    tab.value = 2
+  }
+  if (e.altKey && e.key === '4') {
+    tab.value = 3
+  }
+  if (e.altKey && e.key === '5') {
+    tab.value = 4
+  }
+}
+
+
+const go = () => {
+  if (input.value) {
+    console.log('go')
+    switch(tab.value) {
+      case 0:
+        window.open(`https://www.baidu.com/s?wd=${input.value}`)
+        break
+      case 1:
+        window.open(`https://www.google.com/search?q=${input.value}`)
+        break
+      case 2:
+        window.open(`https://www.bing.com/search?q=${input.value}`)
+        break
+      case 3:
+        window.open(`https://minecraft.fandom.com/zh/wiki/Special:%E6%90%9C%E7%B4%A2?query=${input.value}`)
+        break
+      case 4:
+        window.open(`https://search.mcmod.cn/s?key=${input.value}`)
+        break
+    }
+  }
+  input.value = ''
+}
 </script>
 
 <template>
-  <!-- <div style="margin: 0 15px;">
-    <n-tabs type="segment" v-model:value="tab" animated class="tabs1">
-      <n-tab name="baidu">
-        百度
-      </n-tab>
-      <n-tab name="bing">
-        必应
-      </n-tab>
-      <n-tab name="google">
-        谷歌
-      </n-tab>
-      <n-tab name="wiki">
-        MC Wiki
-      </n-tab>
-      <n-tab name="modmc">
-        ModMC
-      </n-tab>
-    </n-tabs>
-  </div> -->
   <div>
     <n-space justify="center">
-      <div v-for="(item, i) in list" :key="i" class="list_item" @click="change_tab(i)" :class="i==tab?'activated':''">
-        {{ item.name }}
+      <div v-for="(item, i) in list" :key="i" @click="change_tab(i)">
+        <div class="list_item_keyword">Alt + {{ i + 1 }}</div>
+        <div class="list_item" :class="i==tab?'activated':''">{{ item.name }}</div>
       </div>
     </n-space>
   </div>
-  <!-- {{ tab }} -->
   <div class="input_box">
-    <input type="text" class="input">
-    <n-button strong secondary circle class="input_btn" size="large">
+    <input type="text" class="input" @keydown.enter="go" v-model="input">
+    <n-button strong secondary circle class="input_btn" size="large" @click="go" :disabled="input === ''">
       <template #icon>
         <n-icon color="var(--gray-7)"><SubdirectoryArrowLeftRound /></n-icon>
       </template>
@@ -77,12 +102,17 @@ const change_tab = (index: number) => {
 }
 
 .list_item.activated {
-  /* transition: .25s; */
-  /* background: var(--theme-6); */
   background: linear-gradient(to right,#5f87f8,#3860f4);
   animation: myAnimation .2s linear forwards;
-  /* transition: .5s; */
 }
+
+.list_item_keyword {
+  font-size: 1rem;
+  color: var(--gray-5);
+  text-align: center;
+  margin-bottom: 6px;
+}
+
 @keyframes myAnimation {
   from {
     background: var(--bg);
@@ -101,14 +131,12 @@ const change_tab = (index: number) => {
   margin: 20px auto;
   background-color: #FFF;
   max-width: 500px;
-  /* border: 1px solid #ccc; */
   border-radius: 999px;
   padding: 5px;
 }
 
 .input {
   width: 100%;
-  /* max-width: 100%; */
   display: flex;
   height: 40px;
   border: none;
