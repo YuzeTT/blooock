506f51dd19f94fd4b3508089046e420e344434cd
YuzeTT <ziv1024@163.com>
2022-07-28 14:26:09 +0800

    update pwa & add icon

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { useRegisterSW, registerSW } from 'virtual:pwa-register/vue';
+
+useRegisterSW();
+
+const intervalMS = 24 * 60 * 60 * 1000
+
+const updateSW = registerSW({
+  onRegistered(r:any) {
+    r && setInterval(() => {
+      r.update()
+    }, intervalMS)
+  }
+})
 
 createApp(App).use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,31 @@ export default defineConfig({
     Components({
       resolvers: [NaiveUiResolver()]
     }),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      manifest: {
+        name: 'Blooock 起始页',
+        short_name: 'Blooock',
+        display: 'standalone',
+        theme_color: '#3b82f6',
+        background_color: '#f1f2f6',
+        description: '一个专为Minecraft玩家打造的起始页',
+        icons: [
+          {
+            src: './icon.svg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './icon_512.svg',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ],
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
   ]
 })
